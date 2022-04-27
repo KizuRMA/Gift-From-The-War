a11f6418f92c12ab61fcdef38d32c8c38052f4a9
KizuRMA <kd1309678@st.kobedenshi.ac.jp>
2022-04-28 07:02:48 +0900

    コウモリのアニメーションを調整しました。

---
+++ Gift From The War/Assets/enemy/bat/BatController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour
+{
+    BaseState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
