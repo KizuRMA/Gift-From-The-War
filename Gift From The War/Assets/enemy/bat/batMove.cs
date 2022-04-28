26594cd4fe73cc43659336d6f16743fb19735e6b
KizuRMA <84616492+KizuRMA@users.noreply.github.com>
2022-04-28 16:12:41 +0900

    Revert "Ver.0.0.2"

--- Gift From The War/Assets/enemy/bat/batMove.cs
+++ Gift From The War/Assets/enemy/bat/batMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class batMove : MonoBehaviour
+public class batMove : BaseState
 {
     private CharacterController playerCC;
     private CharacterController batCC;
@@ -11,9 +11,13 @@ public class batMove : MonoBehaviour
     Transform rantanTransform;
     [SerializeField] bool moveFlg;
     [SerializeField] float hight;
+    [SerializeField] float forwardAngle;
+    [SerializeField] float playerFromInterval;
+
+    
 
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
         playerCC = GameObject.Find("player").GetComponent<CharacterController>();
         batCC = GetComponent<CharacterController>();
@@ -22,11 +26,37 @@ public class batMove : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
+        //体を前に傾ける
+        Transform _myTrans = this.transform;
+
+        Vector3 _localAngle = _myTrans.localEulerAngles;
+
+        _localAngle.x = forwardAngle;
+
+        _myTrans.eulerAngles = _localAngle;
+
+        //移動する場合
         if (moveFlg)
         {
-            _agent.destination = playerCC.transform.position;
+            Vector3 _playerPos = playerCC.transform.position;
+            Vector3 _myPos = this.transform.position;
+
+            //プレイヤーに近づきすぎない処理
+            float dis = Vector3.Distance(_myPos,_playerPos);
+
+            //プレイヤーとの距離を調べる
+            if(dis <= playerFromInterval)
+            {
+                //近づきすぎている場合
+                _agent.destination = _myPos;
+            }
+            else
+            {
+                //離れている場合
+                _agent.destination = _playerPos;
+            }
         }
 
         rantanTransform.position += new Vector3(0, hight, 0);
